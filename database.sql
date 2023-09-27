e6dc100bb1d4646dc329096f9cf59dd7f67746af
Ameer S <72147027+Ameer2750@users.noreply.github.com>
2023-09-27 10:59:59 +0530

    add database.sql file for reference of tables

---
+++ database.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `projects` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+);
